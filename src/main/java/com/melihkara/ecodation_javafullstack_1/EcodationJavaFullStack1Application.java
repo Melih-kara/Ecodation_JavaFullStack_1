a90acfac6d637882ee882b60139e251a0a319664
Melih-kara <melihkara2001@hotmail.com>
2022-03-05 10:30:26 +0300

    Initial commit

---
+++ src/main/java/com/melihkara/ecodation_javafullstack_1/EcodationJavaFullStack1Application.java
@@ -0,0 +1,13 @@
+package com.melihkara.ecodation_javafullstack_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcodationJavaFullStack1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcodationJavaFullStack1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/melihkara/ecodation_javafullstack_1/EcodationJavaFullStack1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melihkara.ecodation_javafullstack_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcodationJavaFullStack1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
